6063360bc69dfdecf9982788753d3999d3cdbdb7
Max Bo <max@maxbo.me>
2018-02-27 15:32:27 +1000

    Helper functions and more tree progress

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -40,8 +40,37 @@ final case class Config(
 
 object Main extends App {
 
+  implicit class StringOps(self: String) {
+    def quote = self // TODO
+  }
+
   implicit class TreeOps(self: Tree) {
-    def visitTree = visit(self)
+    def visit: String = visitTree(self)
+  }
+
+  implicit class ListOps(self: List[Tree]) {
+    private def formatList[A](xs: List[A]): String = {
+      if (xs.length <= 1) {
+        xs.mkString(", ") // empty lists are going to produce empty strings
+      } else {
+        xs.init.mkString(", ") + " and " + xs.last // safe
+      }
+    }
+
+    private def visitList[A](xs: List[Tree]): String = {
+      formatList(xs.map(_.visit))
+    }
+
+    private def visitListOptional(xs: List[Tree], formatString: String => String = identity): String = {
+      if (xs.isEmpty) {
+        ""
+      } else {
+        formatString(visitList(xs))
+      }
+    }
+
+    def visit: String = visitList(self)
+    def visitFormatNonEmpty(f: String => String): String = visitListOptional(self, f)
   }
 
   // For development
@@ -75,29 +104,107 @@ object Main extends App {
     }
   }
 
-  def visitList(xs: List[Tree]): String = {
-    xs.map(visit).mkString(", ")
-  }
 
-  def visit(tree: Tree): String = {
+  def visitTree(tree: Tree): String = {
     tree match {
-      case _import: Import =>
+      case _import: Import =>{
         "An import"
-      case typeName: Type.Name =>
-        s"typeName.value"
-      case typeApply: Type.Apply =>
-        visitList(typeApply.args)
-      case defnObject: Defn.Object =>
-        f"Object definition ${visit(defnObject.name)} with children ${visitList(defnObject.children)}"
-      case termName: Term.Name =>
-        termName.value
+      }
 
-      case x => f"[HALT] ${x.productPrefix}"
+      case typeName: Type.Name => {
+        typeName.value
+      }
+
+      case typeApply: Type.Apply => {
+        typeApply.args.visitFormatNonEmpty(x => f"args $x")
+      }
+
+      case defnObject: Defn.Object => {
+        val mods = defnObject.mods.visit
+        val name = defnObject.name.visit
+        val templ = defnObject.templ.visit
+
+        f"$mods object $name has $templ"
+      }
+
+      case termName: Term.Name => {
+        termName.value
+      }
+
+      case template: Template => {
+        val early = template.early.visit
+        val inits = template.inits.visit
+        val self = template.self.visit
+        val stats = template.stats.visit
+
+        s"$early $inits $self $stats"
+      }
+
+      case defnTrait: Defn.Trait => {
+        val name = defnTrait.name.visit
+        val tparams = defnTrait.tparams.visitFormatNonEmpty(x => f"has type params $x")
+        val mods = defnTrait.mods.visit
+        val ctor = defnTrait.ctor.visit
+        val template = defnTrait.templ.visit
+
+        f"$mods trait $name $tparams $template $ctor"
+      }
+
+      case defnType: Defn.Type => {
+        val name = defnType.name.visit
+        val body = defnType.body.visit
+        val tparams = defnType.tparams.visit
+        val mods = defnType.mods.visit
+
+        f"$mods type alias $name type $tparams equal to $body"
+      }
+
+      case typeParam: Type.Param => {
+        val name = typeParam.name.visit
+        val mods = typeParam.mods.visit
+        val cbounds = typeParam.cbounds.visit
+        val vbounds = typeParam.vbounds.visit
+        val tbounds = typeParam.tbounds.visit
+
+        f"$mods $name $cbounds $vbounds $tbounds"
+      }
+
+      case typeBounds: Type.Bounds => {
+        val hi = typeBounds.hi.map(x => f"upper bound ${x.visit}").getOrElse("")
+        val lo = typeBounds.hi.map(x => f"lower bound ${x.visit}").getOrElse("")
+
+        f"$hi $lo"
+      }
+
+      case defnClass: Defn.Class => {
+        val ctor = defnClass.ctor.visit
+        val mods = defnClass.mods.visit
+        val templ = defnClass.templ.visit
+        val name = defnClass.name.visit
+        val tparams = defnClass.tparams.visit
+
+        f"$mods class $name $templ $ctor"
+      }
+
+      case modCase: Mod.Case => {
+        "case"
+      }
+
+      case ctorPrimary: Ctor.Primary => {
+        val mods = ctorPrimary.mods.visit
+        val name = ctorPrimary.name.visit
+        val paramss = ctorPrimary.paramss
+
+        s"$mods $name"
+      }
+
+
+      case x => f"[?] ${x.productPrefix} [/?]"
     }
   }
 
   def recursiveExplore(stats: List[Tree])(level: Int): Unit = {
-    println(stats.map(visit).mkString("\n"))
+    println(stats.map(_.visit).mkString("\n"))
   }
 
   def describe(xs: String)(implicit cfg: Config): String = {
