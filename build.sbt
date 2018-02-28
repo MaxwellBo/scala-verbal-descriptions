6d1d5e730487bf501fec3049005ff480e6000f9e
Max Bo <max@maxbo.me>
2018-02-28 14:12:47 +1000

    Add tests extracted from the proposal

--- build.sbt
+++ build.sbt
@@ -3,5 +3,11 @@ version := "0.1.0"
 organization := "Maxwell Bo"
 
 scalaVersion := "2.12.4"
+
 libraryDependencies += "org.scalameta" %% "scalameta" % "3.3.1"
 libraryDependencies += "com.github.scopt" %% "scopt" % "3.7.0"
+
+val scalaTestVersion = "3.0.5"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % scalaTestVersion
+libraryDependencies += "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,50 @@
+import org.scalatest._
+import scala.meta._
+
+class MainSpec extends WordSpec with Matchers {
+
+  "visitTree" should {
+
+    "parse 'ErrorsOr[A]' Defn.Type" in {
+      val errorsOr: Defn.Type = q"type ErrorsOr[A] = ValidatedNel[String, A]"
+
+      val description = "type alias ErrorsOr type A equal to ValidatedNel of String and type A"
+      val summary = "alias ErrorsOr A"
+
+      Main.visitTree(errorsOr) shouldEqual description
+    }
+
+    "parse 'a' Defn.Val" in {
+      val a: Defn.Val = q"val a = 5"
+
+      val description = "val a with value 5"
+      val summary = "val a"
+
+      Main.visitTree(a) shouldEqual description
+    }
+
+    "parse 'Monad' Defn.Trait" in {
+      val monad: Defn.Trait =
+        q"""trait Monad[F[_]] extends Applicative[F] {
+              def one = 1
+              def two = 2
+            }
+         """ // no stripMargin on this quasi-quote weirdly? ~mbo
+
+      val description = "trait Monad with higher type F extending applicative of F with two declarations"
+      val summary = "trait Monad higher F"
+
+      Main.visitTree(monad) shouldEqual description
+    }
+
+    "parse 'Person' Defn.Class" in {
+      val person: Defn.Class = q"case class Person(name: String, address: Address, age: Int)"
+
+      val description = "case class Person containing name String, address Address and age Int"
+      val summaryPreferSymbols = "case class Person with name, address and age"
+      val summaryPreferTypes = "case class Person with String, Address and Int"
+
+      Main.visitTree(person) shouldEqual description
+    }
+  }
+}
